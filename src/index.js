eac16754699621d0160f8612cdaa965e102e47c0
Bohdan Tsybulskyi <b.tsybulskyy@gmail.com>
2018-01-07 22:16:40 +0200

    basic reacr router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+class Bye extends Component {
+  render() {
+    return <div>Bye</div>;
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/bye" component={Bye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
